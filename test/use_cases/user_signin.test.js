b210eecda638246f75d20b1f7d7cdc3ea7cf092a
shweta <shwetzpatil@gmail.com>
2021-03-23 08:55:28 +0000

    Added userRepo and updated signup logic

--- test/use_cases/user_signin.test.js
+++ test/use_cases/user_signin.test.js
@@ -1,15 +1,35 @@
 import { expect } from 'chai'
 
 class User {
-    static isAuthenicated(username, password){
+    static isAuthenicated(username, password, userRepo){
+        const user = userRepo.getUserByUserName(username);
+        console.log(user)
+        if (user && user.username === username && user.password === password){
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+}
+
+class UserRepo {
+    constructor(){
+        this.users = {};
+    }
+
+    getUserByUserName = (username) => {
+      return this.users[username];
+    }
+
+    addUser = (username, password) => {
+        this.users[username] = {username: username, password: password};
         return true;
     }
 }
 
 function signUpUser(username, password, userRepo){
-    userRepo[username] = password;
-    console.log(userRepo)
-    return userRepo;
+    return userRepo.addUser(username, password);
 }
 
 
@@ -17,7 +37,16 @@ describe('User signin', () => {
     it('succesfully authenticate user when given valid username and password', () => {
         const username = 'valid-username';
         const password = 'valid-password';
-        expect(User.isAuthenicated(username, password)).to.equal(true);
+        const userRepo = new UserRepo();
+        signUpUser(username, password, userRepo);
+        expect(User.isAuthenicated(username, password, userRepo)).to.equal(true);
+    })
+
+    it('should fail to authenticate user when given invalid username or password', () => {
+        const username = 'invalid-username';
+        const password = 'invalid-password';
+        const userRepo = new UserRepo();
+        expect(User.isAuthenicated(username, password, userRepo)).to.equal(false);
     })
 
 });
@@ -26,7 +55,22 @@ describe('User signup', () => {
     it('succesfully sign up user when given valid username and password', () => {
         const username = 'valid-username';
         const password = 'valid-password';
-        expect(signUpUser(username, password, {})).to.eql({[`${username}`]: `${password}`});
+        const userRepo = new UserRepo();
+        expect(signUpUser(username, password, userRepo)).to.equal(true);
     })
 
+});
+
+describe('UserRepo', () => {
+    it('adds user to storage and gets user by username', () => {
+        const username = 'valid-username';
+        const password = 'valid-password';
+        const userRepo = new UserRepo();
+        userRepo.addUser(username, password);
+        const expectedUser = userRepo.getUserByUserName(username);
+
+        expect(expectedUser).to.not.equal(null);
+        expect(expectedUser.username).to.equal(username);
+        expect(expectedUser.password).to.equal(password);
+    })
 });
